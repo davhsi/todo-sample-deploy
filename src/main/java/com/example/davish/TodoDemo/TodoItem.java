748db2ea644560c3487176a6c60e4d51faf5635c
davhsi <davish.std@gmail.com>
2025-08-01 14:54:54 +0530

    db logic update

--- src/main/java/com/example/davish/TodoDemo/TodoItem.java
+++ src/main/java/com/example/davish/TodoDemo/TodoItem.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 public class TodoItem {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     
     @NotBlank(message = "Category is required")
